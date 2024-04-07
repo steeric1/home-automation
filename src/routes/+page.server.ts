3eb1f98b5d024c297834147787efa832dd57e0d3
Nikko Kujanpää <nikko.kujanpaa@gmail.com>
2024-04-07 23:03:02 +0300

    feat: write departure times into file with proper format

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,21 +3,14 @@ import fs from "fs/promises";
 import { fail } from "@sveltejs/kit";
 import type { Actions } from "./$types";
 
-import { SECRET_NUMBERS_FILE } from "$env/static/private";
+import { DEPARTURES_FILE } from "$env/static/private";
 
 export const actions: Actions = {
     default: async ({ request }) => {
-        console.log("here");
-
         const data = await request.formData();
-        const nearDepartureTime = data.get("nearDepartureTime");
-
-        if (!nearDepartureTime) {
-            return fail(400, { number: nearDepartureTime, missing: true });
-        }
 
         try {
-            await fs.writeFile(SECRET_NUMBERS_FILE, nearDepartureTime.toString());
+            await handleDepartureTimes(data);
         } catch (err) {
             return fail(500);
         }
@@ -25,3 +18,36 @@ export const actions: Actions = {
         return { success: true };
     }
 };
+
+async function handleDepartureTimes(data: FormData) {
+    const [near, far] = [data.get("nearDepartureTime"), data.get("farDepartureTime")];
+
+    if (near) {
+        const nextNearTimestamp = getNextTimestamp(near.toString()).toISOString();
+        await fs.appendFile(DEPARTURES_FILE, `${nextNearTimestamp} Pusula/Nummela\n`);
+    }
+
+    if (far) {
+        const nextFarTimestamp = getNextTimestamp(far.toString()).toISOString();
+        await fs.appendFile(DEPARTURES_FILE, `${nextFarTimestamp} kauemmaksi\n`);
+    }
+}
+
+function getNextTimestamp(targetTime: string): Date {
+    const [targetHours, targetMinutes] = targetTime.split(":").map(Number);
+    const now = new Date();
+
+    // Create a Date object for today at the target time
+    const nextTimestamp = new Date();
+    nextTimestamp.setHours(targetHours);
+    nextTimestamp.setMinutes(targetMinutes);
+    nextTimestamp.setSeconds(0);
+    nextTimestamp.setMilliseconds(0);
+
+    // If the target time is in the past for today, set it to tomorrow
+    if (nextTimestamp < now) {
+        nextTimestamp.setDate(nextTimestamp.getDate() + 1);
+    }
+
+    return nextTimestamp;
+}

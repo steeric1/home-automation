6257775e1bd4c95441733865dd4d9ecab3804c15
Nikko Kujanpää <nikko.kujanpaa@gmail.com>
2024-04-08 14:52:00 +0300

    refactor POST handling into api endpoint

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,11 +1,11 @@
 import fs from "fs/promises";
 
-import { fail, type ActionFailure } from "@sveltejs/kit";
 import type { Actions, PageServerLoad } from "./$types";
 
 import { DEPARTURES_FILE } from "$env/static/private";
 import type { Departure } from "$lib/types";
-import { departureTimePoiLabels } from "$lib/const";
+import { POST as POST_DEPARTURE } from "./api/departure/+server";
+import { fail } from "@sveltejs/kit";
 
 export const load: PageServerLoad = async ({ params }) => {
     let departuresList: Departure[] | null = null;
@@ -25,65 +25,10 @@ export const load: PageServerLoad = async ({ params }) => {
 };
 
 export const actions: Actions = {
-    submitDepartures: async ({ request }) => {
-        const data = await request.formData();
+    submitDepartures: async (event) => {
+        const response = await POST_DEPARTURE(event);
+        const body = await response.json();
 
-        try {
-            const failure = await handleDepartureTimes(data);
-            if (failure) return failure;
-        } catch (err) {
-            return fail(500);
-        }
-
-        return { success: true };
+        return response.ok ? { success: true } : fail(response.status, body);
     }
 };
-
-async function handleDepartureTimes(data: FormData): Promise<ActionFailure<undefined> | null> {
-    const [near, far, raw] = [
-        data.get("nearDepartureTime")?.toString(),
-        data.get("farDepartureTime")?.toString(),
-        data.get("rawDepartureTime")?.toString()
-    ];
-
-    if (near) {
-        const nextNearTimestamp = getNextTimestamp(near).toISOString();
-        await fs.appendFile(
-            DEPARTURES_FILE,
-            `${nextNearTimestamp} ${departureTimePoiLabels.near}\n`
-        );
-    }
-
-    if (far) {
-        const nextFarTimestamp = getNextTimestamp(far).toISOString();
-        await fs.appendFile(DEPARTURES_FILE, `${nextFarTimestamp} ${departureTimePoiLabels.far}\n`);
-    }
-
-    if (raw) {
-        const departureTimeRegex = /^(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z) (\S.*)/;
-        if (!departureTimeRegex.test(raw)) return fail(400);
-
-        await fs.appendFile(DEPARTURES_FILE, `${raw}\n`);
-    }
-
-    return null;
-}
-
-function getNextTimestamp(targetTime: string): Date {
-    const [targetHours, targetMinutes] = targetTime.split(":").map(Number);
-    const now = new Date();
-
-    // Create a Date object for today at the target time
-    const nextTimestamp = new Date();
-    nextTimestamp.setHours(targetHours);
-    nextTimestamp.setMinutes(targetMinutes);
-    nextTimestamp.setSeconds(0);
-    nextTimestamp.setMilliseconds(0);
-
-    // If the target time is in the past for today, set it to tomorrow
-    if (nextTimestamp < now) {
-        nextTimestamp.setDate(nextTimestamp.getDate() + 1);
-    }
-
-    return nextTimestamp;
-}
--- src/routes/api/departure/+server.ts
+++ src/routes/api/departure/+server.ts
@@ -3,6 +3,7 @@ import fs from "fs/promises";
 import { json, type RequestHandler } from "@sveltejs/kit";
 
 import { DEPARTURES_FILE } from "$env/static/private";
+import { departureTimePoiLabels } from "$lib/const";
 
 export const DELETE: RequestHandler = async ({ request }) => {
     const { timestamp: rTimestamp, poi: rPoi } = await request.json();
@@ -27,5 +28,82 @@ export const DELETE: RequestHandler = async ({ request }) => {
         return json({}, { status: 500 });
     }
 
+    // For some reason json(...) fails with status 204
     return new Response(null, { status: 204 });
 };
+
+export const POST: RequestHandler = async ({ request }) => {
+    const data = await request.formData();
+
+    try {
+        await handleDepartureTimes(data);
+    } catch (err) {
+        if (err instanceof RequestError) {
+            return json({ message: err.message }, { status: err.status });
+        }
+
+        return json(null, { status: 500 });
+    }
+
+    return json(null, { status: 201 });
+};
+
+async function handleDepartureTimes(data: FormData) {
+    const [near, far, raw] = [
+        data.get("nearDepartureTime")?.toString(),
+        data.get("farDepartureTime")?.toString(),
+        data.get("rawDepartureTime")?.toString()
+    ];
+
+    if (near) {
+        const nextNearTimestamp = getNextTimestamp(near).toISOString();
+        await fs.appendFile(
+            DEPARTURES_FILE,
+            `${nextNearTimestamp} ${departureTimePoiLabels.near}\n`
+        );
+    }
+
+    if (far) {
+        const nextFarTimestamp = getNextTimestamp(far).toISOString();
+        await fs.appendFile(DEPARTURES_FILE, `${nextFarTimestamp} ${departureTimePoiLabels.far}\n`);
+    }
+
+    if (raw) {
+        const departureTimeRegex = /^(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z) (\S.*)/;
+        if (!departureTimeRegex.test(raw))
+            throw new RequestError("invalid departure time format", 400);
+
+        await fs.appendFile(DEPARTURES_FILE, `${raw}\n`);
+    }
+
+    return null;
+}
+
+function getNextTimestamp(targetTime: string): Date {
+    const [targetHours, targetMinutes] = targetTime.split(":").map(Number);
+    const now = new Date();
+
+    // Create a Date object for today at the target time
+    const nextTimestamp = new Date();
+    nextTimestamp.setHours(targetHours);
+    nextTimestamp.setMinutes(targetMinutes);
+    nextTimestamp.setSeconds(0);
+    nextTimestamp.setMilliseconds(0);
+
+    // If the target time is in the past for today, set it to tomorrow
+    if (nextTimestamp < now) {
+        nextTimestamp.setDate(nextTimestamp.getDate() + 1);
+    }
+
+    return nextTimestamp;
+}
+
+class RequestError extends Error {
+    status: number;
+
+    constructor(message: string, status: number) {
+        super(message);
+        this.name = this.constructor.name;
+        this.status = status;
+    }
+}
